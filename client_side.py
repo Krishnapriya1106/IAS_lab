0de15afa7e1ec895324fb3d703d635bb43cd3bd9
Krishnapriya1106 <70127285+Krishnapriya1106@users.noreply.github.com>
2021-01-17 17:15:57 +0530

    Delete client_side.py

--- client_side.py
+++
@@ -1,9 +0,0 @@
-import client_stub
-function=input("Enter the function to be used ( Format:<func_name> <argument_1> <arguement_2>.......<arguement_n> ):\n")
-def start():
-    print("Starting the RPC")
-try:
-    string=eval(function+"()")
-except:
-    res=client_stub.middle(function)
-    print("Result:",res)
